0dbd8d86c55aa489db87f43cbfef25d5562f654c
Daniel R Farrell <danielfarrell.r@gmail.com>
2016-10-30 14:34:30 -0600

    Cleaned up JS Formatting

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//3D positioning
-
-window.addEventListener('mousemove', function(e){
-	
-	var hscreen 		= window.innerHeight;
-	var wscreen 		= window.innerWidth;
-	var posY 			= (e.pageY / hscreen * 100) - 50;
-	var posX  			= (e.pageX / wscreen * 100) - 50;
-    var translateY 	= posY / 6;
-	var translateX 	= posX / 6;
-	var rotateX 		= posX / 6;
-	var rotateY 		= posY / 6;
-	var header			= document.querySelector('.transform3d');
-	var title			= document.querySelector('.title');
-	var titleDist   = title.getAttribute('data-distance');
-	
-	title.style.transform = 'translate(' + translateX*titleDist + 'px,' + translateY*titleDist + 'px) rotateX(' + rotateX/titleDist + 'deg) rotateY(' + rotateY/titleDist + 'deg)';
-	title.style.textShadow = '' + (translateX*2)+ 'px ' + (translateY*2) + 'px' + ' 100px rgba(0,0,0,0.2)';
-	
-}, false);
-
 
 $('body').bind('touchstart', function() {});
 
@@ -26,7 +5,7 @@ $(document).ready(function() {
 $('body').css('display', 'none');
 $('body').fadeIn(1500);
 
-  
+
 $('.link').click(function() {
 event.preventDefault();
 newLocation = this.href;
@@ -37,3 +16,26 @@ window.location = newLocation;
 }
 
 });
+
+
+//3D positioning
+
+window.addEventListener('mousemove', function(e){
+	
+	var hscreen = window.innerHeight;
+	var wscreen = window.innerWidth;
+	var posY = (e.pageY / hscreen * 100) - 50;
+	var posX  = (e.pageX / wscreen * 100) - 50;
+ 	var translateY 	= posY / 6;
+	var translateX 	= posX / 6;
+	var rotateX = posX / 6;
+	var rotateY = posY / 6;
+	var header = document.querySelector('.transform3d');
+	var title = document.querySelector('.title');
+	var titleDist   = title.getAttribute('data-distance');
+	
+	title.style.transform = 'translate(' + translateX*titleDist + 'px,' + translateY*titleDist + 'px) rotateX(' + rotateX/titleDist + 'deg) rotateY(' + rotateY/titleDist + 'deg)';
+	title.style.textShadow = '' + (translateX*2)+ 'px ' + (translateY*2) + 'px' + ' 100px rgba(0,0,0,0.2)';
+	
+}, false);
+
